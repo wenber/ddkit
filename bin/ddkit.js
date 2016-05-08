0236a418b836d87fda58561aa60fdb19b3e2cdf5
liuming <liuming@zufangit.cn>
2016-05-08 12:48:20 +0800

    add命令增加参数

--- bin/ddkit.js
+++ bin/ddkit.js
@@ -8,6 +8,9 @@
 var program = require('commander');
 
 program
+    .version('0.0.1')
+
+program
     .command('init')
     .description('初始化一个react项目')
     .action(function(){
@@ -17,10 +20,18 @@ program
 
 program
     .command('add')
-    .description('添加模块')
-    .action(function(name){
+    .option('-r, --react <moduleName>', 'Add react模块')
+    .option('-x, --redux <moduleName>', 'Add redux模块')
+    .description('新增一个react模块（-r）or 新增一个react模块(-x)')
+    .action(function(){
         var add = require('../cli/add');
-        add(name);
+        if (this.react) {
+            add(this.react, 'react');
+        }
+        else if (this.redux) {
+            add(this.redux, 'redux');
+        }
     });
+
 
 program.parse(process.argv);
